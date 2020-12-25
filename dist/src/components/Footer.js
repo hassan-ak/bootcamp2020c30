8e00781a9ac30513a474ef2faccf2645cff81aea
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2020-12-26 03:43:04 +0500

    complete lec
    1:- create App and some component
    Follow "https://docs.bit.dev/docs/tutorials/bit-react-tutorial"
    2:- using bit.dev create a project / componet
    3:- npm install bit-bin --global --no-optional
    4:- bit login
    5:- bit init --package-manager yarn
    6:- bit add src/components/Footer.js
    7:- bit status
    8:- bit import bit.envs/compilers/react --compiler
    9:- bit build
    10:- bit tag --all 0.0.1
    11:- bit status
    12:- bit export hassan-ak.footer
    13:- bit list
    
    bit remove hassan-ak.footer/footer  --remote

---
+++ dist/src/components/Footer.js
@@ -0,0 +1,25 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Footer = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+// React Imports
+var Footer = function Footer() {
+  return (
+    /*#__PURE__*/
+    // Footer Container
+    _react.default.createElement("div", {
+      className: "footer"
+    }, "by:", /*#__PURE__*/_react.default.createElement("strong", null, "Hassan Ali Khan"))
+  );
+};
+
+exports.Footer = Footer;
+
+//# sourceMappingURL=Footer.js.map
